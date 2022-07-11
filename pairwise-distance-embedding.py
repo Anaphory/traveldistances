39087f61ea4b94429f01b3e99bd99c4966919eba
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2022-07-11 22:10:45 +0200

    Mess with code

--- pairwise-distance-embedding.py
+++ pairwise-distance-embedding.py
@@ -1,45 +1,33 @@
-import typing as t
 import sys
-from tqdm import tqdm
+import typing as t
+from heapq import heappop as pop
+from heapq import heappush as push
+from itertools import count
+
+import cartopy.geodesic as geodesic
+import keras
+import keras.backend as K
 import numpy
 import tensorflow as tf
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, LeakyReLU, PReLU, Lambda
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-import numpy as np
-import keras as K
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
-from database import db
-from sqlalchemy import select
-import itertools
-from itertools import count
-import typing as t
-from heapq import heappush as push, heappop as pop
-
-from tqdm import tqdm
-from sqlalchemy import func
-import numpy
-from numpy import pi, cos, inf
-from sqlalchemy import select
-
-import rasterio
-import cartopy.io.shapereader as shpreader
-import shapely.wkb
-
-import cartopy.crs as ccrs
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Lambda
+from keras.layers import PReLU
 from matplotlib import pyplot as plt
-from matplotlib.colors import ListedColormap
-from matplotlib import cm
+from numpy import inf
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sqlalchemy import select
+from tqdm import tqdm
 
 from database import db
-from earth import GEODESIC
+
+GEODESIC: geodesic.Geodesic = geodesic.Geodesic()
 
 
-
-def train_model(X, y, **kwargs):
+def train_model(y, **kwargs):
     # unpack parameters. default values match our model architecture.
     width = kwargs["width"] if "width" in kwargs else 128
     depth = kwargs["depth"] if "depth" in kwargs else 3
@@ -49,9 +37,7 @@ def train_model(X, y, **kwargs):
     plot = kwargs["plot"] if "plot" in kwargs else False
     verbose = kwargs["verbose"] if "verbose" in kwargs else False
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.1, random_state=9
-    )
+    y_train, y_test, _, _ = train_test_split(y, y, test_size=0.1, random_state=9)
 
     latlon_inputs_1 = keras.Input(shape=(2))
     latlon_inputs_2 = keras.Input(shape=(2))
@@ -65,10 +51,11 @@ def train_model(X, y, **kwargs):
         embedding_1 = dout(prelu(dense(embedding_1)))
         embedding_2 = dout(prelu(dense(embedding_2)))
 
-    def euclidean_distance(x1, x2):
+    def euclidean_distance(x1x2):
+        x1, x2 = x1x2
         return K.sum((x1 - x2) * (x1 - x2)) ** 0.5
 
-    euclidean = Lambda(euclidean_distance)(embedding_1, embedding_2)
+    euclidean = Lambda(euclidean_distance)([embedding_1, embedding_2])
 
     exhuming_1 = embedding_1
     exhuming_2 = embedding_2
@@ -93,12 +80,18 @@ def train_model(X, y, **kwargs):
             tf.keras.losses.MeanAbsoluteError(),
             tf.keras.losses.MeanSquaredError(),
             tf.keras.losses.MeanSquaredError(),
-        ]
+        ],
+        optimizer="adam",
     )
+    tf.keras.utils.plot_model(full_model)
+
+    X_train = y_train[:, 1:]
+    model_fp = "%s/%s.hdf5" % ("models", "embed")
+    checkpointer = ModelCheckpoint(model_fp, save_best_only=True)
     es = EarlyStopping(patience=early_stopping)
     history = full_model.fit(
-        X_train,
-        y_train,
+        [X_train[:, 0:2], X_train[2:4]],
+        [y_train[:, 0], y_train[:, 1:3], y_train[:, 3:5]],
         epochs=train_epochs,
         validation_split=0.1,
         callbacks=[checkpointer, es],
@@ -107,9 +100,9 @@ def train_model(X, y, **kwargs):
     val_losses = history.history["val_loss"]
     best_epoch = val_losses.index(min(val_losses)) + 1
 
-    y_pred = model.predict(X)
-    y_pred_train = model.predict(X_train)
-    y_pred_test = model.predict(X_test)
+    y_pred = full_model.predict(y[:, 1:])
+    y_pred_train = full_model.predict(y_train[:, 1:])
+    y_pred_test = full_model.predict(y_test[:, 1:])
     train_mse = mean_squared_error(y_train, y_pred_train)
     test_mse = mean_squared_error(y_test, y_pred_test)
 
@@ -210,6 +203,7 @@ def distances_from_focus(
                     (
                         (TABLES["edges"].c.node1 == spot)
                         & (TABLES["edges"].c.node2 != spot)
+                        & (TABLES["edges"].c.travel_time >= 0)
                     )
                     if filter is True
                     else filter
@@ -239,6 +233,111 @@ def distances_from_focus(
 
 
 if __name__ == "__main__":
+    y = [
+        (
+            1636622.1629435765,
+            -55.78125000000057,
+            -7.25625000000074,
+            -41.343750000000796,
+            -9.697916666667346,
+        ),
+        (
+            980861.3498341956,
+            -59.331250000000466,
+            -4.993750000000787,
+            -53.531250000000625,
+            -3.597916666667473,
+        ),
+        (
+            1539035.1210719983,
+            -40.72513888888889,
+            -5.82097222222222,
+            -51.86041666666728,
+            -0.10625000000088392,
+        ),
+        (
+            2685168.1103357156,
+            -35.23958333333425,
+            -5.781250000000789,
+            -56.906250000000455,
+            -6.014583333334123,
+        ),
+        (
+            909709.7219902322,
+            -59.2937500000005,
+            -5.797916666667383,
+            -53.90430555555556,
+            -5.200138888888887,
+        ),
+        (
+            1391485.1291209944,
+            -49.981250000000614,
+            -2.6562500000007745,
+            -54.7020833333338,
+            4.539583333332416,
+        ),
+        (
+            725329.9977462677,
+            -52.89791666666724,
+            -9.989583333334068,
+            -52.47291666666729,
+            -5.443750000000826,
+        ),
+        (
+            816665.7067046541,
+            -50.793750000000614,
+            -0.7520833333341628,
+            -54.493750000000546,
+            2.7270833333324163,
+        ),
+        (
+            914174.6743733712,
+            -56.1895833333339,
+            -7.264583333334102,
+            -59.96458333333385,
+            -3.585416666667541,
+        ),
+        (
+            762551.2793549526,
+            -49.34375000000057,
+            -0.452083333334123,
+            -46.835416666667385,
+            -1.239583333334167,
+        ),
+        (
+            762551.2793549526,
+            -49.34375000000057,
+            -0.452083333334123,
+            -46.835416666667385,
+            -1.239583333334167,
+        ),
+        (
+            1451798.5695275445,
+            -59.858472222222225,
+            -3.337638888888888,
+            -51.256250000000534,
+            -1.5187500000008711,
+        ),
+        (
+            2347127.421323842,
+            -58.9520833333338,
+            3.6354166666657264,
+            -49.306250000000716,
+            -8.602083333334072,
+        ),
+        (
+            2130689.4799715267,
+            -50.11041666666728,
+            -0.8729166666674217,
+            -59.89597222222223,
+            8.191527777777779,
+        ),
+    ]
+
+    train_model(numpy.array(y), verbose=True, plot=True, model_name="alpha_model")
+
+
+if __name__ == "__new_main__":
     DATABASE, TABLES = db(sys.argv[1])
     nodes = [
         (node, (lon, lat))
@@ -247,17 +346,21 @@ if __name__ == "__main__":
                 TABLES["nodes"].c.node_id,
                 TABLES["nodes"].c.longitude,
                 TABLES["nodes"].c.latitude,
+            ).where(
+                (-60 < TABLES["nodes"].c.longitude)
+                & (TABLES["nodes"].c.longitude < -30)
+                & (-10 < TABLES["nodes"].c.latitude)
+                & (TABLES["nodes"].c.latitude < 10)
             )
         )
     ]
 
-    X, y = [], []
-    for i in tqdm(range(2000)):
+    y = [], []
+    for i in tqdm(range(20)):
         i_start = numpy.random.randint(len(nodes))
         i_end = numpy.random.randint(len(nodes))
         start, lonlat_start = nodes[i_start]
         end, lonlat_end = nodes[i_end]
         dist = distances_from_focus(start, end)
-        X.append([lonlat_start, lonlat_end])
         y.append([dist, lonlat_start, lonlat_end])
-    train_model(X, y, verbose=True, plot=True, model_name="alpha_model")
+        print(dist[end], lonlat_start, lonlat_end)
