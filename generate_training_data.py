1b61097a4b772149ed6cb9aa2c0223afc1f48524
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2022-07-25 11:09:33 +0200

    Add helper scripts

---
+++ generate_training_data.py
@@ -0,0 +1,163 @@
+"""Generate pairwise distance training data."""
+import sys
+import typing as t
+from heapq import heappop as pop
+from heapq import heappush as push
+from itertools import count
+
+import cartopy.geodesic as geodesic
+import numpy
+from numpy import inf
+from sqlalchemy import select
+from tqdm import tqdm
+
+from database import db
+
+GEODESIC: geodesic.Geodesic = geodesic.Geodesic()
+
+
+def distances_from_focus(
+    source,
+    database,
+    tables,
+    filter_sources: t.Optional[t.Set[str]] = None,
+    maximum_dist: float = 5_000_000.0,
+    destinations: t.Set[int] = None,
+    filter_nodes: bool = False,
+    pred: t.Optional = None,
+) -> numpy.array:
+    """Compute shortest distances on a database."""
+    # Dijkstra's algorithm, adapted for our purposes from
+    # networkx/algorithms/shortest_paths/weighted.html,
+    # extended to be an A* implementation
+    seen: t.Dict[t.Tuple[int, int], float] = {source: 0.0}
+    c = count()
+    # use heapq with (distance, label) tuples
+    fringe: t.List[t.Tuple[float, int, t.Tuple[int, int]]] = []
+    # Speeds faster than 3m/s are really rare, even on rivers, so use geodesic
+    # distance with a speed of 3m/s as heuristic
+    push(fringe, (0, next(c), source))
+
+    if filter_sources:
+        filter = tables["edges"].c.source.in_(filter_sources)
+        if filter_nodes:
+            filter &= tables["nodes"].c.node_id > 100_000_000
+    else:
+        if filter_nodes:
+            filter = tables["nodes"].c.node_id > 100_000_000
+        else:
+            filter = True
+
+    dist = t.DefaultDict(lambda: inf)
+
+    while fringe:
+        (d, _, spot) = pop(fringe)
+        if dist.get(spot, numpy.inf) < d:
+            continue  # already found this node, faster, through another path
+        dist[spot] = d
+        if d > maximum_dist:
+            break
+        if destinations is not None and spot in destinations:
+            destinations.remove(spot)
+            if not destinations:
+                break
+
+        for u, cost, data_source, lon, lat in database.execute(
+            select(
+                [
+                    tables["edges"].c.node2,
+                    tables["edges"].c.travel_time,
+                    tables["edges"].c.source,
+                    tables["nodes"].c.longitude,
+                    tables["nodes"].c.latitude,
+                ]
+            )
+            .select_from(
+                tables["edges"].join(
+                    tables["nodes"],
+                    onclause=tables["edges"].c.node2 == tables["nodes"].c.node_id,
+                )
+            )
+            .where(
+                (
+                    (
+                        (tables["edges"].c.node1 == spot)
+                        & (tables["edges"].c.node2 != spot)
+                        & (tables["edges"].c.travel_time >= 0)
+                    )
+                    if filter is True
+                    else filter
+                )
+                & (tables["edges"].c.node1 == spot)
+                & (tables["edges"].c.node2 != spot)
+            )
+        ):
+            if u < 100_000_000 and spot > 100_000_000:
+                # Moving from land to kayak takes 3 hours
+                cost += 3 * 3600
+            vu_dist = dist[spot] + cost
+            if u in dist and vu_dist < dist[u]:
+                if pred:
+                    via = f"via {pred[u]} [{dist[pred[u]]}]"
+                else:
+                    via = ""
+                print(
+                    f"Contradictory paths found: Already reached u={u} at distance {dist[u]}{via} from {data_source}, but now finding a shorter connection {vu_dist} via {spot} [{dist[spot]}]. Do you have negative weights, or a bad heuristic?"
+                )
+            elif u not in seen or vu_dist < seen[u]:
+                seen[u] = vu_dist
+                push(fringe, (vu_dist, next(c), u))
+                if pred is not None:
+                    pred[u] = spot, data_source
+    return dist
+
+
+def more_y(n):
+    more_y = []
+    try:
+        for i in tqdm(range(n)):
+            i_start = numpy.random.randint(len(nodes))
+            start = node_indices[i_start]
+            lonlat_start = nodes[start]
+            dist = distances_from_focus(
+                start, maximum_dist=500_000.0, database=DATABASE, tables=TABLES
+            )
+            for mid, distance in dist.items():
+                if mid not in nodes:
+                    # This can happen if the nodes are filtered down. They are not
+                    # filtered down in the Dijkstra.
+                    continue
+                more_y.append(
+                    [
+                        distance,
+                        lonlat_start[0],
+                        lonlat_start[1],
+                        nodes[mid][0],
+                        nodes[mid][1],
+                    ]
+                )
+    finally:
+        more_y = numpy.array(more_y)
+        try:
+            y = numpy.load(open("distances.npy", "rb"), allow_pickle=False)
+            y = numpy.vstack((y, more_y))
+        except FileNotFoundError:
+            y = more_y
+        numpy.save(open("distances.npy", "wb"), y, allow_pickle=False)
+    return len(more_y)
+
+
+if __name__ == "__main__":
+    DATABASE, TABLES = db(sys.argv[1])
+    nodes = {
+        node: (lon, lat)
+        for node, lon, lat in DATABASE.execute(
+            select(
+                TABLES["nodes"].c.node_id,
+                TABLES["nodes"].c.longitude,
+                TABLES["nodes"].c.latitude,
+            )
+        )
+    }
+    node_indices = list(nodes)
+    print(more_y(2000))
---
+++ plot-embedding-results.py
@@ -0,0 +1,217 @@
+"""Plot embedding model results."""
+import argparse
+
+import cartopy.crs as ccrs
+import cartopy.geodesic as geodesic
+import keras.models
+import matplotlib.cm as cm
+import numpy
+from matplotlib import pyplot as plt
+from matplotlib.colors import Normalize
+from sqlalchemy import select
+from tqdm import tqdm
+
+from database import db
+from generate_training_data import distances_from_focus
+
+
+DATABASE, TABLES = db("sqlite:///all-distances.sqlite")
+GEODESIC: geodesic.Geodesic = geodesic.Geodesic()
+
+lon, lat = 34.08, 45.00
+# distances_from_focus(nearby_node(lon, lat), database=DATABASE, tables=TABLES)
+
+
+def nearby_node(longitude, latitude):
+    lon_scaling_factor = numpy.cos(latitude / 180 * numpy.pi)
+    d_min = numpy.inf
+    for node, lon, lat in DATABASE.execute(
+        select(
+            [
+                TABLES["nodes"].c.node_id,
+                TABLES["nodes"].c.longitude,
+                TABLES["nodes"].c.latitude,
+            ]
+        )
+        .select_from(TABLES["nodes"])
+        .where(
+            ((TABLES["nodes"].c.longitude - longitude) * lon_scaling_factor < 2)
+            & ((TABLES["nodes"].c.longitude - longitude) * lon_scaling_factor > -2)
+            & ((TABLES["nodes"].c.latitude - latitude) < 2)
+            & ((TABLES["nodes"].c.latitude - latitude) > -2)
+        )
+        .order_by(
+            (
+                (TABLES["nodes"].c.longitude - longitude)
+                * (TABLES["nodes"].c.longitude - longitude)
+            )
+            * lon_scaling_factor**2
+            + (TABLES["nodes"].c.latitude - latitude)
+            * (TABLES["nodes"].c.latitude - latitude)
+        )
+    ):
+        d = GEODESIC.inverse((lon, lat), (longitude, latitude))[0, 0]
+        if d < d_min:
+            d_min = d
+            node_min = node
+        if d > 2 * d_min:
+            break
+    else:
+        raise ValueError("No habitable place nearby.")
+    return node_min
+
+
+def distance_raster(start, ends):
+    distances = {}
+    for node, dist in distances_from_focus(
+        start,
+        database=DATABASE,
+        tables=TABLES,
+        maximum_dist=numpy.inf,
+        destinations=ends,
+    ).items():
+        lon, lat = DATABASE.execute(
+            select(
+                [
+                    TABLES["nodes"].c.longitude,
+                    TABLES["nodes"].c.latitude,
+                ]
+            )
+            .select_from(TABLES["nodes"])
+            .where(TABLES["nodes"].c.node_id == node)
+        ).one()
+        distances[lon, lat] = dist
+    return distances
+
+
+X = 100
+Y = 100
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("stored_model")
+    args = parser.parse_args()
+
+    full_model = keras.models.load_model(args.stored_model)
+
+    embedding_model = keras.Model(
+        inputs=[full_model.input[0]],
+        outputs=[full_model.get_layer("euclidean_distance_in_embedding").input[0]],
+    )
+
+    mesh_coords = (
+        numpy.stack(
+            numpy.meshgrid(numpy.linspace(-180, 180, 720), numpy.linspace(-90, 90, 360))
+        )
+        .transpose((1, 2, 0))
+        .reshape(-1, 2)
+    )
+    results = embedding_model.predict(mesh_coords)
+    if results.shape[1] > 25:
+        results = results[:, :25]
+
+    layout = int(results.shape[1] ** 0.5)
+    fig = plt.figure()
+    ref = None
+    for i, component in enumerate(results.T):
+        ax = fig.add_subplot(
+            (results.shape[1] - 1) // layout + 1,
+            layout,
+            i + 1,
+            projection=ccrs.Mollweide(),
+            sharex=ref,
+            sharey=ref,
+        )
+        if ref is None:
+            ref = ax
+        ax.coastlines()
+        plt.imshow(
+            component.reshape((360, 720)),
+            extent=(-180, 180, -90, 90),
+            transform=ccrs.PlateCarree(),
+            origin="lower",
+        )
+    plt.show()
+
+    min_lon, max_lon, min_lat, max_lat = 32.6, 54.9, 38.9, 49.3
+    mesh_coords = numpy.stack(
+        numpy.meshgrid(
+            numpy.linspace(min_lon, max_lon, X), numpy.linspace(min_lat, max_lat, Y)
+        )
+    ).transpose((1, 2, 0))
+    positions = {
+        "crh": (34.08, 45.00),
+        "nog": (43.17, 44.90),
+        "kum": (47.00, 43.00),
+        "azj": (46.47, 40.98),
+        "tuk": (59.18, 37.09),
+        "kaa": (63.32, 39.98),
+    }
+    coords = numpy.array(list(positions.values()))
+    nodes = [nearby_node(lon, lat) for lon, lat in coords]
+    stack = numpy.stack(
+        numpy.broadcast_arrays(mesh_coords[None, :, :, :], coords[:, None, None, :])
+    )
+    starts, ends = numpy.broadcast_arrays(
+        mesh_coords[None, :, :, :], coords[:, None, None, :]
+    )
+    results = full_model.predict([starts.reshape(-1, 2), ends.reshape(-1, 2)])
+
+    distances = []
+    common_nodes = None
+    for i, node in enumerate(tqdm(nodes)):
+        d = distance_raster(node, nodes[:i] + nodes[i + 1 :])
+        if common_nodes:
+            common_nodes &= set(d)
+        else:
+            common_nodes = set(d)
+        distances.append(d)
+
+    common_distances = {
+        point: sum(d[point] for d in distances) for point in tqdm(common_nodes)
+    }
+    x = [x for x, y in common_distances]
+    y = [y for x, y in common_distances]
+    c = list(common_distances.values())
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection=ccrs.Mollweide())
+    ax.coastlines()
+    ax.set_global()
+
+    cmap = cm.get_cmap("viridis")
+    normalizer = Normalize(0, 12_000_000)
+
+    plt.pcolormesh(
+        mesh_coords[..., 0],
+        mesh_coords[..., 1],
+        results[0].reshape((6, X, Y)).sum(0),
+        transform=ccrs.PlateCarree(),
+    )
+    plt.scatter(x, y, 1, c=c, transform=ccrs.PlateCarree(), cmap=cmap, norm=normalizer)
+    plt.show()
+
+    cmap = cm.get_cmap("viridis")
+    normalizer = Normalize(0, 12_000_000)
+
+    im = cm.ScalarMappable(norm=normalizer)
+    fig = plt.figure()
+    for i, (lat, lon) in enumerate(tqdm(positions.values())):
+        ax = fig.add_subplot(2, 3, i + 1, projection=ccrs.Mollweide())
+        ax.coastlines()
+        ax.pcolormesh(
+            mesh_coords[..., 0],
+            mesh_coords[..., 1],
+            results[0].reshape((6, X, Y))[i],
+            transform=ccrs.PlateCarree(),
+            cmap=cmap,
+            norm=normalizer,
+        )
+        x = [x for x, y in common_nodes]
+        y = [y for x, y in common_nodes]
+        c = [distances[i][n] for n in common_nodes]
+        plt.scatter(
+            x, y, 1, c=c, transform=ccrs.PlateCarree(), cmap=cmap, norm=normalizer
+        )
+        ax.scatter([lon], [lat], transform=ccrs.PlateCarree())
+        ax.set_title(list(positions)[i])
+
+    plt.show()
